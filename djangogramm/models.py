0ebd30b1b4061cdd02009ac4b543679e63e170da
Viktor Bazatyn <bazatinviktor@gmail.com>
2024-04-15 21:08:55 +0300

    changed models

--- djangogramm/models.py
+++ djangogramm/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
@@ -10,38 +8,29 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars')
     email = models.EmailField(unique=True)
 
-    def __str__(self):
-        return self.email
+
+def get_image_path(instance, filename):
+    post_id = instance.id
+    return f'posts/{post_id}/{filename}'
 
 
 class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=255, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts')
+    image = models.ImageField(upload_to=get_image_path)
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
-    comments = models.ManyToManyField('Comment', related_name='comments', blank=True)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='post_comments')
     created_at = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return f"Post by {self.author.email}"
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
 
 class Comment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField(blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return f"Comment by {self.author.email}"
-
-
-class Picture(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image = models.ImageField(upload_to='pictures')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"Picture for post {self.post.id}"
