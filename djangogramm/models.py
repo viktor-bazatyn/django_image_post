3ad9b9f31d84ea636837895dbda9ec01626b2378
Viktor Bazatyn <bazatinviktor@gmail.com>
2024-04-16 22:11:08 +0300

    changed get_image_path and add uuid

--- djangogramm/models.py
+++ djangogramm/models.py
@@ -1,9 +1,13 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from faker.utils.text import slugify
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     biography = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars')
     email = models.EmailField(unique=True)
@@ -11,10 +15,12 @@ class User(AbstractUser):
 
 def get_image_path(instance, filename):
     post_id = instance.id
+    filename = slugify(filename, allow_unicode=True)
     return f'posts/{post_id}/{filename}'
 
 
 class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=255, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=get_image_path)
@@ -24,12 +30,14 @@ class Post(models.Model):
 
 
 class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
 
 
 class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.TextField(blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
